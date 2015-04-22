4ebbe47eaa20fccdd95dcb298bcdd1a94f8e0286
赖智祥 <xiang135love@sina.cn>
2015-04-22 16:17:23 +0800

    stringjavafileobject

--- src/com/lzx/StringJavaFileObject.java
+++ src/com/lzx/StringJavaFileObject.java
@@ -1,8 +1,7 @@
 package com.lzx;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.URI;
 
 import javax.tools.JavaFileObject;
@@ -12,7 +11,6 @@ public class StringJavaFileObject extends SimpleJavaFileObject {
 
 	private CharSequence content;
 	 
-	protected final ByteArrayOutputStream bos = new ByteArrayOutputStream();
     public StringJavaFileObject(String className, CharSequence content) {
         super(URI.create("string:///" + className.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE);
         this.content = content;
@@ -24,14 +22,4 @@ public class StringJavaFileObject extends SimpleJavaFileObject {
         return content;
     }
 
-    public byte[] getBytes() {
-        return bos.toByteArray();
-    }
-    
-	@Override
-	public OutputStream openOutputStream() throws IOException {
-		return bos;
-	}
-    
-    
 }
